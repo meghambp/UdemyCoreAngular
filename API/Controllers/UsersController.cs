42a5664b4552e89ccacf34d43f4c99ca73528196
meghambp <58705212+meghambp@users.noreply.github.com>
2021-12-28 11:57:20 -0500

    End of section 3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Mvc;
 using API.Data;
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [EnableCors("Policy1")]
     public class UsersController : ControllerBase
     {
         private readonly DataContext _context;
@@ -16,6 +18,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
--- API/Program.cs
+++ API/Program.cs
@@ -7,6 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(
+    options =>
+        {
+            options.AddPolicy("Policy1",
+                builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200/")
+                    .AllowAnyHeader()
+                    .AllowAnyOrigin();
+                });
+
+});
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new() { Title = "API", Version = "v1" });
@@ -23,8 +35,24 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
     app.UseDeveloperExceptionPage();
 }
-
 app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors();
+/*x => 
+    { 
+        /*x
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("http://localhost:4200/");
+        x.AddPolicy("Policy1",
+                builder =>
+                {
+                    builder.WithOrigins("http://example.com",
+                                        "http://www.contoso.com");
+                });
+    });*/
+    
+
 
 app.UseAuthorization();
 
